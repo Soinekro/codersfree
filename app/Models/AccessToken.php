f54672d2cbfa3b4b8b7edfdcf1fe17cb1f84d393
Elder <echumaceroj@ucvvirtual.edu.pe>
2022-02-04 18:46:32 -0500

    configuracion de cliente para login

---
+++ app/Models/AccessToken.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class AccessToken extends Model
+{
+    protected $fillable =['user_id','service_id','access_token','refresh_token','expires_at'];
+    use HasFactory;
+
+    //relacion 1 a 1
+
+    public function user(){
+        return $this->hasOne(AccessToken::class);
+    }
+}
